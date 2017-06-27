87a82726d8546ca4c293405ac5510aa44f5ac525
cmhnk <corinne@automatedinsights.com>
2017-06-27 12:48:31 -0400

    Try moving javascript stuff to server to see if that fixes the cors thing

--- server/index.js
+++ server/index.js
@@ -34,6 +34,40 @@ if (isDeveloping) {
   app.use(express.static(path.join(__dirname, '/../dist')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+    import './styles/base.scss';
+
+    import $ from 'jquery';
+    $(document).ready(function() {
+      var $form = $('.nag-form');
+      console.log('hello');
+
+      $form.submit( function(event){
+        alert("Thank you for submitting your response! The robots are working...");
+        event.preventDefault();
+        console.log('yo!');
+        var data = {};
+        $(".nag").each(function() {
+          data[$(this).attr("name")] = Number($(this).val());
+        });
+        data["firstname"] = $(".nag-string").val();
+
+        console.log(data);
+
+        var wordsmith = require('wordsmith-node-sdk')('process.env.WORDSMITH_KEY', 'Corinne');
+        console.log(wordsmith);
+
+        wordsmith.projects.find('nag-simulator')
+          .then(function(project) {
+            return project.templates.find('nagging-parent');
+          })
+          .then(function(template) {
+            return template.generate(data);
+          })
+          .then(function(content) {
+            $( ".four " ).text(content)
+          })
+      });
+    });
   });
 }
 
